fa9a705ec455fc518b01b7bc10892f92d2327128
carlitowey4 <carlosespigarestriano@gmail.com>
2023-11-13 10:04:31 +0100

    updated

--- 01-basics/ut-4-vanillajs-3/T4_P8 - Promise/promise.js
+++ 01-basics/ut-4-vanillajs-3/T4_P8 - Promise/promise.js
@@ -1,21 +1,50 @@
+const database={
+  users: [
+      {id: 1, name: "Usuario 1"},
+      {id: 2, name: "Usuario 2"},
+      {id: 3, name: "Usuario 3"}
+  ],
+};
 
-function getElementById(id) {
-    return new Promise((resolve, reject) => {
-      // Simulamos una conexión a la base de datos con un retraso de 2 segundos
+export function getElementById(id) {
+  return new Promise((resolve, reject) => {
+      // Simulando una búsqueda en la base de datos
       setTimeout(() => {
-        const data = [
-          { id: 1, name: "Usuario 1" },
-          { id: 2, name: "Usuario 2" },
-          { id: 3, name: "Usuario 3" },
-        ];
-  
-        const element = data.find(item => item.id === id);
-  
-        if (element) {
-          resolve(element);
-        } else {
-          reject("User not found");
-        }
+          const user = database.users.find(user => user.id === id);
+
+          if (user) {
+              resolve(user);
+          } else {
+              reject("User not found");
+          }
       }, 2000);
-    });
-  }
+  });
+}
+
+export function getUserByName(name) {
+  return new Promise((resolve, reject) => {
+      // Simulando una búsqueda en la base de datos
+      setTimeout(() => {
+          const user = database.users.find(user => user.name === name);
+
+          if (user) {
+              resolve(user);
+          } else {
+              reject("User not found");
+          }
+      }, 2000);
+  });
+}
+
+export function calculatePowPromise(numbers) {
+  return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (numbers.length === 0) {
+          reject("The length of the array is 0");
+      } else {
+          const result = numbers.map(num => num ** 2);
+          resolve(result);
+      }
+    }, 2000);
+  });
+}
--- 01-basics/ut-4-vanillajs-3/T4_P8 - Promise/promise.test.js
+++ 01-basics/ut-4-vanillajs-3/T4_P8 - Promise/promise.test.js
@@ -1,47 +1,52 @@
-import * as desctructure from "./promise";
-
-const database={
-    users: [
-        {id: 1, name: "Usuario 1"},
-        {id: 2, name: "Usuario 2"},
-        {id: 3, name: "Usuario 3"}
-    ],
-};
+import { getElementById, getUserByName, calculatePowPromise } from "./promise";
 
 describe("DataBase Promises - selectUserById", () => {
     it ("should gets user with id = 2",  async () => {
         const result = await getElementById(2);
-    const expected = { id: 2, name: "Usuario 2" };
-    assert.deepEqual(result, expected);
+        const expected = { id: 2, name: "Usuario 2" };
+        expect(result).toEqual(expected);
     });
     it ("should gets User not found with id = 5", async () => {
         try {
             await getElementById(5);
-            assert.fail("Promise should have rejected");
-          } catch (error) {
-            assert.equal(error, "User not found");
-          }
+            // If the promise does not reject, fail the test
+            expect.fail("Promise should have rejected");
+        } catch (error) {
+            expect(error).toEqual("User not found");
+        }
     });
 
 });
 
 
 describe("DataBase Promises - selectUserByName", () => {
-    it ("should gets user with name = Usuario 3", () => {
-
+    it ("should gets user with name = Usuario 3", async () => {
+        const user = await getUserByName("Usuario 3");
+        expect(user).toEqual({ id: 3, name: "Usuario 3" });
     });
-    it ("should gets User not found with name = John", () => {
-
+    it ("should gets User not found with name = John", async () => {
+        try {
+            await getUserByName("John");
+          } catch (error) {
+            expect(error).toEqual("User not found");
+          }
     });
 
 });
 
-describe("Calculate Pow Promise", () => {
-    it ("should get [1, 4, 9, 16, 25] with [1, 2, 3, 4, 5]", () => {
-
+describe("Calculate Pow Promise",() => {
+    it ("should get [1, 4, 9, 16, 25] with [1, 2, 3, 4, 5]",  async() => {
+        const result = await calculatePowPromise([1, 2, 3, 4, 5]);
+        const expected = [1, 4, 9, 16, 25];
+        expect(result).toEqual(expected);
     });
-    it ("should get The length arrays is 0 with []", () => {
-
+    it ("should get The length arrays is 0 with []",  async() => {
+        try {
+            await calculatePowPromise([]);
+            expect.fail("Promise should have rejected");
+        } catch (error) {
+            expect(error).toEqual("The length of the array is 0");
+        }
     });
 
 });
